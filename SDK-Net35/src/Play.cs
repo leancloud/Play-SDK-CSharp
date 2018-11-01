c17ecc0591a6ab1c9564345e37960ab91913d10c
onerain <onerain88@gmail.com>
2018-11-01 10:28:04 +0800

    fix: 修复 windows 下的证书问题

--- SDK-Net35/src/Play.cs
+++ SDK-Net35/src/Play.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
@@ -190,6 +192,7 @@ namespace LeanCloud.Play {
                     break;
             }
             this.PlayState = PlayState.CONNECTING;
+            ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
             new Thread(() =>
             {
                 var client = new WebClient();
@@ -927,5 +930,30 @@ namespace LeanCloud.Play {
                 return instance;
             }
         }
-	}
+
+        public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            bool isOk = true;
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if (sslPolicyErrors != SslPolicyErrors.None)
+            {
+                for (int i = 0; i < chain.ChainStatus.Length; i++)
+                {
+                    if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                    {
+                        chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                        chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                        chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                        chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                        bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                        if (!chainIsValid)
+                        {
+                            isOk = false;
+                        }
+                    }
+                }
+            }
+            return isOk;
+        }
+    }
 }
