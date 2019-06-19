384de6b712e67a98f1815bcec20314f74a003e06
oneRain <onerain88@gmail.com>
2019-06-19 12:21:18 +0800

    chore: 完善 PlayObject

--- Assets/Source/PlayArray.cs
+++ Assets/Source/PlayArray.cs
@@ -1,77 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace LeanCloud.Play {
-    /// <summary>
-    /// 数组类结构，实现 IList
-    /// </summary>
-    public class PlayArray : IList<object> {
-        internal List<object> Data {
+    public class PlayArray : IList {
+        internal IList Data {
             get; private set;
         }
 
-        public int Count => ((IList<object>)Data).Count;
+        public bool IsFixedSize => Data.IsFixedSize;
 
-        public bool IsReadOnly => ((IList<object>)Data).IsReadOnly;
+        public bool IsReadOnly => Data.IsReadOnly;
 
-        public object this[int index] { get => ((IList<object>)Data)[index]; set => ((IList<object>)Data)[index] = value; }
+        public int Count => Data.Count;
 
-        public int IndexOf(object item) {
-            return ((IList<object>)Data).IndexOf(item);
-        }
+        public bool IsSynchronized => Data.IsSynchronized;
 
-        public void Insert(int index, object item) {
-            ((IList<object>)Data).Insert(index, item);
-        }
+        public object SyncRoot => Data.SyncRoot;
 
-        public void RemoveAt(int index) {
-            ((IList<object>)Data).RemoveAt(index);
-        }
-
-        public void Add(object item) {
-            ((IList<object>)Data).Add(item);
-        }
-
-        public void Clear() {
-            ((IList<object>)Data).Clear();
-        }
-
-        public bool Contains(object item) {
-            return ((IList<object>)Data).Contains(item);
-        }
-
-        public void CopyTo(object[] array, int arrayIndex) {
-            ((IList<object>)Data).CopyTo(array, arrayIndex);
-        }
-
-        public bool Remove(object item) {
-            return ((IList<object>)Data).Remove(item);
-        }
-
-        public IEnumerator<object> GetEnumerator() {
-            return ((IList<object>)Data).GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() {
-            return ((IList<object>)Data).GetEnumerator();
-        }
-
-        public PlayArray() {
-            Data = new List<object>();
-        }
+        public object this[int index] { get => Data[index]; set => Data[index] = value; }
 
         public PlayArray(int capacity) {
             Data = new List<object>(capacity);
         }
 
-        public static PlayArray ToPlayArray(List<object> data) {
-            if (data == null) {
-                return null;
+        public PlayArray() : this(0) {
+
+        }
+
+        public PlayArray(IList data) : this() {
+            if (data != null) {
+                foreach (var d in data) {
+                    Data.Add(d);
+                }
             }
-            var playArray = new PlayArray {
-                Data = data
-            };
-            return playArray;
+        }
+
+        public int Add(object value) {
+            return Data.Add(value);
+        }
+
+        public void Clear() {
+            Data.Clear();
+        }
+
+        public bool Contains(object value) {
+            return Data.Contains(value);
+        }
+
+        public int IndexOf(object value) {
+            return Data.IndexOf(value);
+        }
+
+        public void Insert(int index, object value) {
+            Data.Insert(index, value);
+        }
+
+        public void Remove(object value) {
+            Data.Remove(value);
+        }
+
+        public void RemoveAt(int index) {
+            Data.RemoveAt(index);
+        }
+
+        public void CopyTo(Array array, int index) {
+            Data.CopyTo(array, index);
+        }
+
+        public IEnumerator GetEnumerator() {
+            return Data.GetEnumerator();
         }
     }
 }
